ee033c144315c58fec3d1d7360b8f7ac17aba71f
Aly <aschmidt36@my.wctc.edu>
2015-10-15 12:58:53 -0500

    prefected what the SalesReportData data string looks like

--- src/ajs/parkinggarageapp/SalesReportData.java
+++ src/ajs/parkinggarageapp/SalesReportData.java
@@ -7,7 +7,7 @@ import java.text.NumberFormat;
  * @author Alyson
  */
 public class SalesReportData{
-
+    private static final String DASHED = "======================================";
 
     private String salesReportData(String garageName, double hours, double fee, int cars){
         NumberFormat curr = NumberFormat.getCurrencyInstance();
@@ -15,6 +15,9 @@ public class SalesReportData{
         final String newLine = "\n";
         StringBuilder receiptData = new StringBuilder();
         receiptData.append(garageName).append(newLine);
+        receiptData.append(DASHED).append(newLine);
+        receiptData.append("Daily Garage Totals").append(newLine);
+        receiptData.append(DASHED).append(newLine);
         receiptData.append("TotalCars: ").append(cars).append(newLine);
         receiptData.append("Total Hours Billed: ").append(h.format(hours)).append(newLine);
         receiptData.append("Collected: ").append(curr.format(fee)).append(newLine);
@@ -26,6 +29,10 @@ public class SalesReportData{
         return salesReportData(garageName, hours, fee, cars);
     }
     
-    
+    public static void main(String[] args) {
+        SalesReportData data = new SalesReportData();
+        System.out.println(data.getSalesReportData("Herbies", 32.56, 92.34532, 11));
+        
+    }
     
 }
